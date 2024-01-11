305f998778476b05137c5d3898eaf409df8583f4
1Ricardo5 <114586483+1Ricardo5@users.noreply.github.com>
2024-01-11 12:46:11 -0500

    challenge 4

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,13 +6,16 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
    
     // Start is called before the first frame update
     void Start()
     {
-     
-     
-     Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);   
+     SpawnEnemyWave(waveNumber);
+     Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);  
+     Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
     
     
     }
@@ -20,7 +23,10 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+       enemyCount = FindObjectsOfType<Enemy>().Length;
+       if (enemyCount == 0) {  waveNumber++; SpawnEnemyWave(waveNumber); 
+       Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);}
     }
 
 
@@ -31,4 +37,21 @@ public class SpawnManager : MonoBehaviour
        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
        return randomPos;
     }
+
+     void SpawnEnemyWave(int enemiesToSpawn)
+      {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(),
+            enemyPrefab.transform.rotation);
+        }
+      }
+
+
+
+
+
+
 }
+
+
